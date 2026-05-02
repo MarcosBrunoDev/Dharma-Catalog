e6aa93d3e971cd9f3c854a3f107d7a6a69b12bbe
MarcosBrunoDev <marcosbrunodharmait@gmail.com>
2026-05-02 01:52:26 -0300

    fix middleware para produccion

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
-export async function proxy(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET })
   const isAdminPage = req.nextUrl.pathname.startsWith('/admin')
   const isLoginPage = req.nextUrl.pathname.startsWith('/login')
